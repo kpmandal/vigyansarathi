82118d806bbbd7389673f7eb318cd3b8ed1a4e32
kpmandal <30822431+kpmandal@users.noreply.github.com>
2025-07-12 15:32:32 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
-
 // pages/_app.js
 
-import '../styles/globals.css'; // <-- THIS IS THE CRITICAL LINE
+// This line imports your global stylesheet, which is essential for Tailwind CSS to work.
+import '../styles/globals.css';
 
+// This imports the Layout component we created, which contains the Header and Footer.
+import Layout from '../components/Layout';
+
+// This is the main App component.
+// 'Component' represents the current page being displayed (e.g., index.js, [slug].js).
+// 'pageProps' are the props passed to that page (e.g., from getStaticProps).
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  
+  // By wrapping the Component with Layout, every page on your site will
+  // automatically get the Header and Footer.
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
+
